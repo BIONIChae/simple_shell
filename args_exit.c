4965d9abe792d09a5cbc9773fbe90a93baf7a6fb
Dee2002 <deemarasha77@gmail.com>
2023-05-24 09:45:50 -0700

    exit file fix

--- args_exit.c
+++ args_exit.c
@@ -1,27 +1,44 @@
 #include "unix.h"
-
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 /**
- * _exiting_ - Exits the shell using an int.
- * @count: The arg count.
- * @vec: Double pointer to the array of strings.
- *
- * Return: 0 on success.
- */
+* _exiting_ - Exits the shell using an int.
+* @count: The arg count.
+* @vec: Double pointer to the array of strings.
+*
+* Return: 0 on success.
+*/
 int _exiting_(int count, char **vec)
 {
-	int status = atoi(vec[1]);
+int status = 0;
+int i;
 
-	while (vec[count] != NULL)
-	{
-		write(STDOUT_FILENO, vec[count], strlen(vec[count]));
-		count++;
+if (count < 2)
+{
+write(STDERR_FILENO, "Error: Incorrect number of arguments\n",
+strlen("Error: Incorrect number of arguments\n"));
+exit(EXIT_FAILURE);
+}
 
-	}
-	if (count < 2)
-	{
-		perror("Error: Incorrect number of arguments\n");
-		exit(status);
-	}
-	return (0);
+status = atoi(vec[1]);
+
+for (i = 2; i < count; i++)
+{
+write(STDOUT_FILENO, vec[i], strlen(vec[i]));
+write(STDOUT_FILENO, " ", 1);
+}
+
+write(STDOUT_FILENO, "\n", 1);
+
+if (status != 0 && status != EXIT_SUCCESS)
+exit(EXIT_FAILURE);
+
+exit(status);
+/*Reachable code*/
+return (0);
 }
